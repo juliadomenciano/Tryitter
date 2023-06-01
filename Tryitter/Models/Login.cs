212b464042ba76e7026effc5ec4a14b7be0ff85b
aejepsen <aejepsen@yahoo.com.br>
2023-06-01 14:07:10 -0300

    feat: Testes Class Student

--- Tryitter/Models/Login.cs
+++ Tryitter/Models/Login.cs
@@ -5,8 +5,10 @@ namespace tryitter.Models
   public class Login
   {
     [Required]
+    [EmailAddress]
     public string? Email { get; set; }
     [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public string? Password { get; set; }
   }
 }
--- Tryitter/Models/Student.cs
+++ Tryitter/Models/Student.cs
@@ -6,10 +6,14 @@ namespace tryitter.Models
   {
     public int StudentId { get; set; }
     [Required]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters long")]
+    [MaxLength(50)]
     public string? Name { get; set; }
     [Required]
+
     public string? Email { get; set; }
     [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public string? Password { get; set; }
     public string? Status { get; set; }
     public int ModuleId { get; set; }
