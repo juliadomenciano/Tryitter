0180df1ef40c89876f96b5274c01fed9255e0438
aejepsen <aejepsen@yahoo.com.br>
2023-06-02 19:26:54 -0300

    Criação testes para post

--- Tryitter/Models/Post.cs
+++ Tryitter/Models/Post.cs
@@ -6,11 +6,19 @@ namespace tryitter.Models
   public class Post
   {
     public int PostId { get; set; }
+
+    [MaxLength(50)]
+    [MinLength(3)]
+    [Required]
     public string? Title { get; set; }
+    
     [MaxLength(300)]
+    [MinLength(3)]
+    [Required]
     public string? Text { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+
     [ForeignKey("StudentId")]
     public int StudentId { get; set; }
     public virtual Student? Student { get; set; }
